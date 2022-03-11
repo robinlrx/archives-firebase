e5da881618a84fa5eb9b94e3f48c8466437ddb5d
robinlrx <robin@monkey-lab.io>
2022-03-11 16:19:32 +0100

    update collection value of firebase on click

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 /* Add JavaScript code here! */
 console.log('Hello World! You did it! Welcome to Snowpack :D');
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc, updateDoc, doc, increment } from "firebase/firestore";
 import { app } from '/firebase-config.js';
 
+//constants
 const db = getFirestore(app);
 const list = document.getElementsByClassName('list')[0]
+const yesButton = document.getElementById('yes')
+const noButton = document.getElementById('no')
 
-// Get data from murder collections
-const querySnapshot = await getDocs(collection(db, "murder"));
-querySnapshot.forEach((doc) => {
-	const yesValue = doc.data().yes
-	list.innerHTML += yesValue
+// Get data from all murder collections
+// const querySnapshot = await getDocs(collection(db, "murder"));
+// querySnapshot.forEach((doc) => {
+// 	const yesValue = doc.data().yes
+// 	const noValue = doc.data().no
+// });
+
+// get data of one collection
+const userRef = doc(db, "murder", "nbz24O9VmlyMaxGnRQuc");
+console.log(userRef)
+
+yesButton.addEventListener("click", () => {
+	updateDoc(userRef, {
+		yes: increment(1)
+	});
+});
+
+noButton.addEventListener("click", () => {
+	updateDoc(userRef, {
+		no: increment(1)
+	});
 });
