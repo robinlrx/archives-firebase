55f160342dcea20fcc3afe11bfdc4645c4c09386
robinlrx <robin@monkey-lab.io>
2022-03-11 10:55:18 +0100

    firebase

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 /* Add JavaScript code here! */
 console.log('Hello World! You did it! Welcome to Snowpack :D');
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { app } from '/firebase-config.js';
+
+const db = getFirestore(app);
+// const list = document.getElementsByClassName('list')[0]
+
+// Get data from murder collections
+const querySnapshot = await getDocs(collection(db, "murder"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
